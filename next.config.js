b668f1516bd6c2e51f33786a8379bac6c16e9ce9
Ewa Potorecka <epotorecka@primark.ie>
2026-03-06 21:59:23 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'safe-web'
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
