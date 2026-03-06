9b021764a15f15fe2804b6a314d14b738951f898
Ewa Potorecka <epotorecka@primark.ie>
2026-03-06 22:15:37 +0200

    config

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,6 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
-export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
   return {
--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'safe-web'
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
