be738929ffb0ca0bdbdf8af5b0ebb2d1d5e960aa
Ewa Potorecka <epotorecka@primark.ie>
2026-03-06 21:56:02 +0200

    config

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
-
+export const dynamic = 'force-static'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
