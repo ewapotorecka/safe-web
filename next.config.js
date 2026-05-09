912184192cb315403405336b95996443a698035e
Michael Korotchenko <135420876+scottishwidow@users.noreply.github.com>
2026-05-09 13:09:19 +0300

    fix: serve mobile association files from well-known (#6)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
 
 };
 
