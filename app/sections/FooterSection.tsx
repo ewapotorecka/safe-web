f9457f6fee2eb2d4bbc928055c0bb72b959b8403
Ewa Potorecka <epotorecka@primark.ie>
2026-06-14 11:02:56 +0200

    feat: add producthunt link in footer

--- app/sections/FooterSection.tsx
+++ app/sections/FooterSection.tsx
@@ -26,7 +26,7 @@ export function FooterSection({ t }: FooterSectionProps) {
           </a>
         </div>
         <p>{t.footer.rights}</p>
-      </div>
+        <a href="https://www.producthunt.com/products/safe-4/reviews/new?utm_source=badge-product_review&utm_medium=badge&utm_source=badge-safe&#0045;4" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/product_review.svg?product_id=1244328&theme=light" alt="Safe - One&#0032;tap&#0032;to&#0032;tell&#0032;everyone&#0032;you&#0039;re&#0032;okay | Product Hunt" style={{ width: "250px", height: "54px" }} width="250" height="54" /></a>      </div>
     </footer>
   );
 }
